b6d5a1492b5b1f5eb829e032e88be8d3c887f7aa
Kasper <kasperkh.kh@gmail.com>
2021-04-12 21:33:03 +0200

    Ability to get csv file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,27 +3,44 @@
   windows_subsystem = "windows"
 )]
 
+use csv;
+
 mod cmd;
 
-fn main() {
-  tauri::AppBuilder::new()
-    .invoke_handler(|_webview, arg| {
-      use cmd::Cmd::*;
-      match serde_json::from_str(arg) {
-        Err(e) => {
-          Err(e.to_string())
+fn handle_cmd(command: cmd::Cmd) -> Result<(), String> {
+  use cmd::Cmd::*;
+  match command {
+    ImportCsv { file_path } => {
+      println!("{}", file_path);
+      let mut rdr = match csv::Reader::from_path(file_path) {
+        Ok(reader) => reader,
+        Err(e) => return Err("Error opening csv: ".to_string() + &e.to_string()),
+      };
+      let mut i = 0;
+      for result in rdr.records() {
+        let record = match result {
+          Ok(record) => record,
+          Err(e) => return Err("Error reading record: ".to_string() + &e.to_string()),
+        };
+        if i == 0 {
+          println!("{:?}", record);
+          // println!("{}", i);
         }
-        Ok(command) => {
-          match command {
-            // definitions for your custom commands from Cmd here
-            MyCustomCommand { argument } => {
-              //  your command code
-              println!("{}", argument);
-            }
-          }
-          Ok(())
-        }
+        i += 1;
       }
+      return Ok(());
+    }
+  }
+}
+
+fn main() {
+  tauri::AppBuilder::default()
+    .invoke_handler(|_webview, arg| -> Result<(), String> {
+      let command = match serde_json::from_str(arg) {
+        Err(e) => return Err(e.to_string()),
+        Ok(command) => command,
+      };
+      handle_cmd(command)
     })
     .build()
     .run();
--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,39 @@
 <script>
-	let name = ''
+  import * as tauri from 'tauri/api/tauri'
+  import * as dialog from 'tauri/api/dialog'
+
+  async function importCsv() {
+    const filePath = await dialog.open({
+      filter: 'csv',
+      multiple: false,
+    })
+    tauri.invoke({
+      cmd: 'importCsv',
+      file_path: filePath,
+    })
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial" target="_blank">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+<style lang="sass">
+  main
+    text-align: center
+    padding: 1em
+    max-width: 240px
+    margin: 0 auto
+  :global(body)
+    margin: 0
+    font-family: Arial, Helvetica, sans-serif
+    font-size: 18px
+  @media (min-width: 640px)
+    main
+      max-width: none
 </style>
+
+<main>
+  <h1>Riddle</h1>
+  <button on:click={importCsv}>Import</button>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial" target="_blank">Svelte tutorial</a> to learn how
+    to build Svelte apps.
+  </p>
+</main>
