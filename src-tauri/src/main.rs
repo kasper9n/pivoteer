c4d344216d76fd17e61aaf9af268877c4d62db4b
Kasper <kasperkh.kh@gmail.com>
2022-01-20 01:41:49 +0100

    Use new Tauri menu creation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,11 @@
   windows_subsystem = "windows"
 )]
 
+use std::thread;
 use tauri::api::{dialog, shell};
-use tauri::{command, CustomMenuItem, Menu, MenuItem, Submenu, WindowBuilder, WindowUrl};
+use tauri::{
+  command, CustomMenuItem, Menu, MenuEntry, MenuItem, Submenu, Window, WindowBuilder, WindowUrl,
+};
 
 mod cmd;
 
@@ -15,66 +18,17 @@ macro_rules! throw {
   }};
 }
 
-fn custom_menu(name: &str) -> CustomMenuItem {
-  let c = CustomMenuItem::new(name.to_string(), name);
-  return c;
-}
-
 #[command]
-fn error_popup(msg: String) {
-  println!("Error popup: {}", msg);
-  dialog::message("Error", msg);
+fn error_popup(msg: String, win: Window) {
+  eprintln!("Error: {}", msg);
+  thread::spawn(move || {
+    dialog::message(Some(&win), "Error", msg);
+  });
 }
 
 fn main() {
-  let menu = Menu::new()
-    .add_submenu(Submenu::new(
-      // on macOS first menu is always app name
-      &ctx.package_info().name,
-      Menu::new()
-        .add_native_item(MenuItem::About(&ctx.package_info().name.clone()))
-        .add_native_item(MenuItem::Separator)
-        .add_native_item(MenuItem::Services)
-        .add_native_item(MenuItem::Separator)
-        .add_native_item(MenuItem::Hide)
-        .add_native_item(MenuItem::HideOthers)
-        .add_native_item(MenuItem::ShowAll)
-        .add_native_item(MenuItem::Separator)
-        .add_native_item(MenuItem::Quit),
-    ))
-    .add_submenu(Submenu::new(
-      "File",
-      Menu::new().add_item(custom_menu("New").accelerator("cmdOrControl+N")),
-    ))
-    .add_submenu(Submenu::new("Edit", {
-      let mut menu = Menu::new();
-      menu = menu.add_native_item(MenuItem::Undo);
-      menu = menu.add_native_item(MenuItem::Redo);
-      menu = menu.add_native_item(MenuItem::Separator);
-      menu = menu.add_native_item(MenuItem::Cut);
-      menu = menu.add_native_item(MenuItem::Copy);
-      menu = menu.add_native_item(MenuItem::Paste);
-      #[cfg(not(target_os = "macos"))]
-      {
-        menu = menu.add_native_item(MenuItem::Separator);
-      }
-      menu = menu.add_native_item(MenuItem::SelectAll);
-      menu
-    }))
-    .add_submenu(Submenu::new("View", Menu::new()))
-    .add_submenu(Submenu::new(
-      "Window",
-      Menu::new()
-        .add_native_item(MenuItem::Minimize)
-        .add_native_item(MenuItem::Zoom),
-    ))
-    .add_submenu(Submenu::new(
-      "Help",
-      Menu::new().add_item(custom_menu("Learn More")),
-    ))
-    .add_native_item(MenuItem::Copy);
-
   let ctx = tauri::generate_context!();
+
   tauri::Builder::default()
     .create_window("main".to_string(), WindowUrl::default(), |win, webview| {
       let win = win
@@ -89,7 +43,55 @@ fn main() {
       return (win, webview);
     })
     .invoke_handler(tauri::generate_handler![error_popup, cmd::generate,])
-    .menu(menu)
+    .menu(Menu::with_items([
+      #[cfg(target_os = "macos")]
+      MenuEntry::Submenu(Submenu::new(
+        &ctx.package_info().name,
+        Menu::with_items([
+          MenuItem::About(ctx.package_info().name.clone()).into(),
+          MenuItem::Separator.into(),
+          MenuItem::Services.into(),
+          MenuItem::Separator.into(),
+          MenuItem::Hide.into(),
+          MenuItem::HideOthers.into(),
+          MenuItem::ShowAll.into(),
+          MenuItem::Separator.into(),
+          MenuItem::Quit.into(),
+        ]),
+      )),
+      MenuEntry::Submenu(Submenu::new(
+        "File",
+        Menu::with_items([CustomMenuItem::new("New", "New")
+          .accelerator("cmdOrControl+N")
+          .into()]),
+      )),
+      MenuEntry::Submenu(Submenu::new(
+        "Edit",
+        Menu::with_items([
+          MenuItem::Undo.into(),
+          MenuItem::Redo.into(),
+          MenuItem::Separator.into(),
+          MenuItem::Cut.into(),
+          MenuItem::Copy.into(),
+          MenuItem::Paste.into(),
+          #[cfg(not(target_os = "macos"))]
+          MenuItem::Separator.into(),
+          MenuItem::SelectAll.into(),
+        ]),
+      )),
+      MenuEntry::Submenu(Submenu::new(
+        "View",
+        Menu::with_items([MenuItem::EnterFullScreen.into()]),
+      )),
+      MenuEntry::Submenu(Submenu::new(
+        "Window",
+        Menu::with_items([MenuItem::Minimize.into(), MenuItem::Zoom.into()]),
+      )),
+      MenuEntry::Submenu(Submenu::new(
+        "Help",
+        Menu::with_items([CustomMenuItem::new("Learn More", "Learn More").into()]),
+      )),
+    ]))
     .on_menu_event(|event| {
       let event_name = event.menu_item_id();
       event.window().emit("menu", event_name).unwrap();
