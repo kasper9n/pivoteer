b01c5e35224f66a845fe9f5b1747721da24509b1
Kasper <kasperkh.kh@gmail.com>
2023-01-17 11:09:43 +0100

    Working landr pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,85 @@
 use bigdecimal::BigDecimal;
+use chrono::NaiveDate;
 use csv_pipeline::{Error, Pipeline, Transformer};
+use serde::Deserialize;
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
 
-// fn read_csvs<P: AsRef<Path>>(file_paths: Vec<P>) -> Vec<InputStream> {
-//   file_paths
-//     .iter()
-//     .map(|file_path| read_csv(file_path))
-//     .collect()
-// }
+#[derive(Deserialize)]
+struct Settings {
+	landr: Vec<String>,
+}
+impl Settings {
+	fn open(file_path: &str) -> Self {
+		let file = File::open(file_path).unwrap();
+		let reader = BufReader::new(file);
+		let settings = serde_json::from_reader(reader).unwrap();
+		settings
+	}
+}
 
-// fn read_folder<P: AsRef<Path>>(folder_path: P) -> Vec<InputStream> {
-//   let file_paths = read_dir(folder_path)
-//     .unwrap()
-//     .map(|entry| entry.unwrap().path())
-//     .collect();
-//   read_csvs(file_paths)
-// }
+fn landr(file_path: &str) -> Pipeline {
+	Pipeline::from_path(file_path)
+		.unwrap()
+		.validate(|headers, row| match headers.get_field(&row, "Share %") {
+			Some("100") => Ok(()),
+			Some(_) => Err(Error::InvalidField("Share % is not 100".to_string())),
+			None => {
+				let date_field = headers.get_field(&row, "Payment Date").unwrap();
+				let date = NaiveDate::parse_from_str(date_field, "%Y-%m-%d").unwrap();
+				if date <= NaiveDate::from_ymd_opt(2022, 3, 8).unwrap() {
+					Ok(())
+				} else {
+					Err(Error::MissingColumn("Share %".to_string()))
+				}
+			}
+		})
+		.add_col("Period", |headers, row| {
+			let payment_date = headers.get_field(&row, "Payment Date").unwrap();
+			let date = NaiveDate::parse_from_str(payment_date, "%Y-%m-%d").unwrap();
+			let period = date.format("%Y-%m").to_string();
+			Ok(period)
+		})
+		.rename_col("Quantity of sales or streams", "Sales or streams")
+		.rename_col("Net earnings (USD)", "Gross Royalties")
+		.select(vec![
+			"Period",
+			"UPC",
+			"ISRC",
+			"Store",
+			"Store service",
+			"Sales or streams",
+			"Gross Royalties",
+		])
+}
 
 fn main() {
-	let csv = Pipeline::from_path("/Volumes/GoogleDrive/Shared drives/Lacuna/Financial/Sales Reports/Landr/earnings-report-2022-9.csv")
-  .unwrap()
-  .validate_col("Share %", |pct| match pct {
-    "100" => Ok(()),
-    _ => Err(Error::InvalidField("Share % is not 100".to_string())),
-  })
-  .transform_into(|| vec![
-    Transformer::new("UPC").keep_unique(),
-    Transformer::new("ISRC").keep_unique(),
-    Transformer::new("Store").keep_unique(),
-    Transformer::new("Store service").keep_unique(),
-    Transformer::new("Sales or streams").from_col("Quantity of sales or streams").sum(0 as u64),
-    Transformer::new("Royalties").from_col("Net earnings (USD)").sum(BigDecimal::from(0))
-    // Transformer::new("Royalties").from_col("Net earnings (USD)").reduce(
-    //   |accumulator, current| {
-    //     let score: f64 = current.parse().unwrap();
-    //     Ok(accumulator + score)
-    //   },
-    //   0.0,
-    // ),
-  ])
-  .collect_into_string()
-  .unwrap();
+	let settings_path = match env::args().nth(1) {
+		Some(arg) => arg,
+		None => {
+			eprintln!("No Settings.json argument given");
+			return;
+		}
+	};
+	let settings = Settings::open(&settings_path);
+
+	let lands_pipelines: Vec<_> = settings.landr.iter().map(|x| landr(&x)).collect();
+
+	let csv = Pipeline::from_pipelines(lands_pipelines)
+		.transform_into(|| {
+			vec![
+				Transformer::new("Period").keep_unique(),
+				Transformer::new("UPC").keep_unique(),
+				Transformer::new("ISRC").keep_unique(),
+				Transformer::new("Store").keep_unique(),
+				Transformer::new("Store service").keep_unique(),
+				Transformer::new("Sales or streams").sum(0 as i64),
+				Transformer::new("Gross Royalties").sum(BigDecimal::from(0)),
+			]
+		})
+		.collect_into_string()
+		.unwrap();
 
 	println!("{csv}");
 }
